6aff95ed0adbe1d659a51a40b9e899387981d33f
Ivan Demidov <scrum@list.ru>
2025-03-04 17:00:55 +0300

    new iteration

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
-import VideoProcessor from "./VideoProcessor";
-import KeypointVisualizer from "./KeypointVisualizer";
-import Logger from "./Logger";
-
-// Устанавливаем уровни логирования
-Logger.enabledLevels = [Logger.logLevels.info, Logger.logLevels.error];
-
-const VIDEO_PATH = "./GX010064-420-trim.mov";
-
-// Создаем экземпляр VideoProcessor и запускаем обработку видео
-const videoProcessor = new VideoProcessor(VIDEO_PATH);
-videoProcessor.process().then(() => {
-  // После завершения обработки видео, создаем экземпляр KeypointVisualizer и визуализируем ключевые точки
-  const keypointVisualizer = new KeypointVisualizer(VIDEO_PATH);
-  keypointVisualizer.visualize();
-});
+const VIDEO_PATH = "../videos/data/GX010064-420-trim.mov";
